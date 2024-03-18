90b741aa16ed9d5dbfcc6a47002c68d2cceb5ba7
Samuel Dapena Ramos <80526198+Samudr00@users.noreply.github.com>
2024-03-18 18:24:14 +0100

    1 Commit
    Creación de proyecto en spring

---
+++ sdr/src/main/java/tfc/sdr/SdrApplication.java
@@ -0,0 +1,13 @@
+package tfc.sdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdrApplication.class, args);
+	}
+
+}
