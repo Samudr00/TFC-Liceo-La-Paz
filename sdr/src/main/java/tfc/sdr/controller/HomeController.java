6279b9802a249aa3d669aad9ae1bc8ad16d0c3ee
Samuel Dapena Ramos <80526198+Samudr00@users.noreply.github.com>
2024-04-09 17:51:46 +0200

    Add files via upload
    Creación de Controller, primera clase Modelo y primera vista

---
+++ sdr/src/main/java/tfc/sdr/controller/HomeController.java
@@ -0,0 +1,13 @@
+package tfc.sdr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String mostrarHome() {
+		return "eventos/home";
+	}
+}
---
+++ sdr/src/main/java/tfc/sdr/model/Evento.java
@@ -0,0 +1,77 @@
+package tfc.sdr.model;
+
+import java.util.Date;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+public class Evento {
+	
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	private Date fecha;
+	private Integer numeroParticipantes;
+	private String ubicacion;
+	@Enumerated(value = EnumType.STRING)
+	private Verificacion verificacion;
+	private Integer contador;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Integer getNumeroParticipantes() {
+		return numeroParticipantes;
+	}
+	public void setNumeroParticipantes(Integer numeroParticipantes) {
+		this.numeroParticipantes = numeroParticipantes;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public Verificacion getVerificacion() {
+		return verificacion;
+	}
+	public void setVerificacion(Verificacion verificacion) {
+		this.verificacion = verificacion;
+	}
+	public Integer getContador() {
+		return contador;
+	}
+	public void setContador(Integer contador) {
+		this.contador = contador;
+	}
+	
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fecha=" + fecha
+				+ ", numeroParticipantes=" + numeroParticipantes + ", ubicacion=" + ubicacion + ", verificacion="
+				+ verificacion + ", contador=" + contador + "]";
+	}
+
+	
+}
